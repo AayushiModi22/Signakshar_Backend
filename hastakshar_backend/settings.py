c196dc83507e01b3b09a82f4a4439021a2a3baa0
rajvi <rajvigajjar2003@gmail.com>
2024-06-18 16:18:03 +0530

    final june 18

--- hastakshar_backend/settings.py
+++ hastakshar_backend/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR / 'mainapp' / 'email-format' / 'otp-template'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'hastakshar_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'mainapp' / 'email-format'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
