058639757834cf7852b967d77531280a0996c7df
AayushiModi22 <139226294+AayushiModi22@users.noreply.github.com>
2024-07-05 16:40:16 +0530

    Update settings.py

--- hastakshar_backend/settings.py
+++ hastakshar_backend/settings.py
@@ -79,15 +79,26 @@ WSGI_APPLICATION = 'hastakshar_backend.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'hastakshar',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  # Change this to your MySQL server's hostname or IP address
+#         'PORT': '3307',       # Change this to your MySQL server's port
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hastakshar',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # Change this to your MySQL server's hostname or IP address
-        'PORT': '3307',       # Change this to your MySQL server's port
-    }
+        'NAME': 'signakshar',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_EUHuRflCHDzhA37zq_5',
+        'HOST': 'mysql-31695c3c-signakshar-qit.j.aivencloud.com', 
+        'PORT': '25587',      
+    }
 }
 
 
