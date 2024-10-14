4736017080241a9564166160e0e6ce28264d1c5c
AayushiModi22 <139226294+AayushiModi22@users.noreply.github.com>
2024-10-14 18:20:47 +0530

    Delete hastakshar_backend directory

--- hastakshar_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('mainapp.urls')),
-    # path('auth/api/login/google/', GoogleLoginApi.as_view(), name="GoogleLoginApi"),
-
-]
